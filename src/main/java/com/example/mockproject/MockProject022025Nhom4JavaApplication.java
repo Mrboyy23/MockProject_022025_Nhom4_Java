f370d0a91d05cb2c6a97059525a1ecb44a29e9a8
Mrboyy23 <nguyenduyduong.duong2003@gmail.com>
2025-03-11 10:57:40 +0700

    init project

---
+++ src/main/java/com/example/mockproject/MockProject022025Nhom4JavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockProject022025Nhom4JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockProject022025Nhom4JavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mockproject/MockProject022025Nhom4JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockProject022025Nhom4JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
